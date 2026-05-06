f26f649ade8c0ceb9aeda1f594a7475a737d23ad
vijaysingh7878 <vijaysinghshekhawat9928@gmail.com>
2026-05-06 12:45:44 +0530

    Created Note model and implemented CRUD controllers for notes

---
+++ controller/noteController.js
@@ -0,0 +1,82 @@
+import Note from "../model/noteModel.js";
+import User from "../model/userModel.js";
+
+export const createNote = async (req, res) => {
+  try {
+    const { title, content } = req.body;
+    if (!title || !content) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+    const userId = req.user.userId;
+
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    const note = new Note({ title, content, userId });
+    await note.save();
+    res.status(201).json({ message: "Note created successfully", note });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+export const getNotes = async (req, res) => {
+  try {
+    const userId = req.user.userId;
+
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    const notes = await Note.find({ userId });
+    res.status(200).json({ message: "Notes retrieved successfully", notes });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const updateNote = async (req, res) => {
+  try {
+    const userId = req.user.userId;
+    const { id } = req.params;
+    const { title, content } = req.body;
+
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    const note = await Note.findOneAndUpdate(
+      { _id: id, userId },
+      { title, content },
+      { new: true },
+    );
+
+    if (!note) {
+      return res.status(404).json({ message: "Note not found" });
+    }
+
+    res.status(200).json({ message: "Note updated successfully", note });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+export const deleteNote = async (req, res) => {
+  try {
+    const userId = req.user.userId;
+    const { id } = req.params;
+
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    const note = await Note.findOneAndDelete({ _id: id, userId });
+
+    if (!note) {
+      return res.status(404).json({ message: "Note not found" });
+    }
+
+    res.status(200).json({ message: "Note deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
---
+++ model/noteModel.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const noteSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+});
+const Note = mongoose.model("Note", noteSchema);
+export default Note;
