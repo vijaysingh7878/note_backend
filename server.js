2cdb1caf1b80236a180174a627e27ae3675a1104
vijaysingh7878 <vijaysinghshekhawat9928@gmail.com>
2026-05-06 12:00:01 +0530

    Backend setup with Express, MongoDB, cors and dotenv configuration

---
+++ server.js
@@ -0,0 +1,31 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(
+  cors({
+    origin: "*", 
+  }),
+);
+
+
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log("Error connecting to MongoDB", err);
+  });
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
